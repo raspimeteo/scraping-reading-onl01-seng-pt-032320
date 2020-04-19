fcf7fc1af77570edd6b8ce2b093665b540ce43f0
John Zonneveld <jjczon@gmail.com>
2020-04-19 16:12:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,3 +6,6 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
+courses.each do |course|
+  puts course.text.strip
+end
