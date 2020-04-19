ca3fdda3b29d87c8af95e338b1f6942740701ada
John Zonneveld <jjczon@gmail.com>
2020-04-19 16:11:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,5 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
